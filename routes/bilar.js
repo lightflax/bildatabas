407eb54823e8f286690d7015f87a1816e5eec401
webbmaster73 <webbmaster73@gmail.com>
2023-03-25 21:06:44 +0100

    p

--- routes/bilar.js
+++ routes/bilar.js
@@ -42,10 +42,10 @@ router.delete("/:id", function (req, res, next) {
 
 
 
-router.put("/bilar",function (req, res, next) {
-  BilModel.findByIdAndRemove(req.body,  function (err, post) {
+router.put("/", function (req, res, next) {
+  //req.body är innehållet i requestobjektet, dvs en json med en bil
+  BilModel.findByIdAndUpdate(req.body, function (err, post) {
     if (err) return next(err);
-    res.json(post);
+    res.json(post); //Här skickar vi tillbaka datan vi skickar in i databasen, om skrivningen gick bra
   });
-  res.json(req.user)
-})
+});
