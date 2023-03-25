59379a38862f3f6ff73c4d36444edd86cbbfc3a4
webbmaster73 <webbmaster73@gmail.com>
2023-03-25 20:53:46 +0100

    new

--- routes/bilar.js
+++ routes/bilar.js
@@ -44,7 +44,7 @@ router.delete("/:id", function (req, res, next) {
 
  router.put("/", function (req, res, next) {
   //req.body är innehållet i requestobjektet, dvs en json med en bil
-  BilModel.findByIdAndUpdate(req.body, {new: true}, function (err, data) {
+  BilModel.findByIdAndUpdate(req.body, function (err, data) {
     if (err) return next(err);
     res.json(data); //Här skickar vi tillbaka datan vi skickar in i databasen, om skrivningen gick bra
   });
